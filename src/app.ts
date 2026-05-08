3f15fd44a5b537fb9842ce8e9ade8f96348c990b
RyanSoar3s <ryansoares.dev@gmail.com>
2026-05-08 17:07:24 -0300

    Add CORS support and update environment configuration

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import { healthRouter } from './routes/health.js';
 import { menuRouter } from './routes/menu.js';
+import { env } from './config/env.js';
 
 export const app = express();
 
+app.use(cors({
+  credentials: true,
+  origin: env.ORIGIN || "localhost:4200"
+
+}));
 app.use(express.json());
 app.use('/api', healthRouter);
 app.use('/api', menuRouter);
--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { NODE_ENV = 'development', PORT = '3000', DATABASE_URL } = process.env;
+const { ORIGIN = "", NODE_ENV = 'development', PORT = '3000', DATABASE_URL } = process.env;
 
 if (!DATABASE_URL) {
   throw new Error("DATABASE_URL not defined in environment.");
@@ -10,6 +10,7 @@ if (!DATABASE_URL) {
 }
 
 export const env = {
+  ORIGIN,
   nodeEnv: NODE_ENV,
   port: Number(PORT),
   databaseUrl: DATABASE_URL
