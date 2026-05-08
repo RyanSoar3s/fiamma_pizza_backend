e8db22c8969597ab301cf7b9fcc5927eb0ebe27f
RyanSoar3s <ryansoares.dev@gmail.com>
2026-05-08 15:02:02 -0300

    INITIAL COMMIT

---
+++ src/config/env.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { NODE_ENV = 'development', PORT = '3000', DATABASE_URL } = process.env;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL not defined in environment.");
+
+}
+
+export const env = {
+  nodeEnv: NODE_ENV,
+  port: Number(PORT),
+  databaseUrl: DATABASE_URL
+
+};
